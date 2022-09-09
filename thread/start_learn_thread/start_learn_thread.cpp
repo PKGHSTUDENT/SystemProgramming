9e6f9376b50a6e82e5f052fd06798eb7409be2c3
dobrodelete <kirkon3004@gmail.com>
2022-09-10 00:25:33 +0300

    add thread parsing code

---
+++ thread/start_learn_thread/start_learn_thread.cpp
@@ -0,0 +1,57 @@
+﻿// Знакомство работы с WIN32 
+// Работа с потоками
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <thread>
+#include <string>
+#include <process.h>
+#include <Windows.h>
+using namespace std;
+
+volatile int n;
+
+DWORD WINAPI Add(LPVOID iNum) {
+    cout << "Thread is started." << endl;
+    n += (int)iNum;
+    cout << "Thread is finished." << endl;
+
+    return 0;
+}
+
+UINT WINAPI thread(void* pString)
+{
+    int i = 1;
+    char* pLexema;
+    pLexema = strtok((char*)pString, " ");
+
+    while (pLexema != NULL)
+    {
+        cout << "Thread find the lexema " << i << " : " << pLexema << endl;
+        pLexema = strtok(NULL, " ");
+        i++;
+    }
+}
+
+int main()
+{
+    int inc = 10;
+    HANDLE hThread;
+    DWORD IDThread;
+
+    cout << "n = " << n << endl;
+    // Запускаем поток Add
+    hThread = CreateThread(NULL, 0, Add, (void*)inc, 0, &IDThread);
+    if (hThread == NULL)
+        return GetLastError();
+
+    // Ждём, пока поток Add закончит работу
+    WaitForSingleObject(hThread, INFINITE);
+
+    // Закрываем дескриптор потока Add
+    CloseHandle(hThread);
+
+    cout << "n = " << n << endl;
+
+    return 0;
+}
