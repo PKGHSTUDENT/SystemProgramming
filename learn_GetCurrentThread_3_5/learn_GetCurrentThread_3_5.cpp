e54c5f600a9758719ca84629414e3c03915ca6f6
dobrodelete <kirkon3004@gmail.com>
2022-09-10 00:30:11 +0300

    thread

---
+++ learn_GetCurrentThread_3_5/learn_GetCurrentThread_3_5.cpp
@@ -0,0 +1,20 @@
+﻿// Изучение функции GetCurrentThread.
+//
+
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	HANDLE hThread;
+
+	hThread = GetCurrentThread();
+
+	cout << hThread << endl;
+
+	cin.get();
+
+	return 0;
+}
